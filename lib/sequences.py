63bfeced0c28ab5bcb6422b4fdd96e7219ae2a39
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-12-25 18:22:02 -0600

    Complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,14 @@
 
 def print_fibonacci(length):
     pass
+    fibonacci_sequence = []
+    x = 0
+    for x in range(length):
+        if x == 0:
+            fibonacci_sequence.append(0)
+        elif x == 1:
+            fibonacci_sequence.append(1)
+        else:
+            next = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next)
+    print(fibonacci_sequence)
